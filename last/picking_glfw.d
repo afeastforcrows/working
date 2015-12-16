a65664c894ce98d73ea88317bc6a94ca6a07f467
afeastforcrows <afeastforcrows@gmail.com>
2015-12-16 10:41:13 -0800

    Basic scene graph begun.

--- last/picking_glfw.d
+++ last/picking_glfw.d
@@ -1,2 +1,3 @@
 picking_glfw.o: picking_glfw.cpp GLFWApp.h GFXMath.h GFXExtra.h \
-  GLSLShader.h transformations.h PlyModel.h FaceList.h glut_teapot.h
+  GLSLShader.h transformations.h PlyModel.h FaceList.h glut_teapot.h \
+  binary-search-tree/Tree.h binary-search-tree/TreeNode.h
